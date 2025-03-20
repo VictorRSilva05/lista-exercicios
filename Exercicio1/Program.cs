9e7d14a2348ea3031e9bfe6a4d7be27ad04a1730
Victor R. Silva <victorr.silva005@gmail.com>
2025-03-20 14:35:54 -0300

    Exercício 1 concluído

--- Exercicio1/Program.cs
+++ Exercicio1/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Insira a altura da caixa: ");
+            float altura = float.Parse(Console.ReadLine());
+
+            Console.Write("Insira a largura da caixa: ");
+            float largura = float.Parse(Console.ReadLine());
+
+            Console.Write("Insira o comprimento da caixa: ");
+            float comprimento = float.Parse(Console.ReadLine());
+
+            float volume = altura * largura * comprimento;
+
+            Console.WriteLine($"\nO volume da caixa é {volume}cm³");
         }
     }
 }
