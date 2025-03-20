facd98b9b3855a3e26eecc00d5af380d73d3573c
Victor R. Silva <victorr.silva005@gmail.com>
2025-03-20 14:48:45 -0300

    Exercicio 4 concluído

---
+++ Exercicio4/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Exercicio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira a temperatura em Celsius: ");
+            double celsius = double.Parse(Console.ReadLine());
+
+            double fahrenheit = (celsius * 9 / 5) + 32;
+
+            Console.WriteLine($"\nA temperatura em Fahrenheit é {fahrenheit}°F");
+        }
+    }
+}
