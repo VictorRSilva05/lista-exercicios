7908af98a47d651741580092d19966404f3ca661
Victor R. Silva <victorr.silva005@gmail.com>
2025-03-20 14:45:51 -0300

    Exercício 3 concluído

---
+++ Exercicio3/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira a quilometragem inicial: ");
+            double quilometragemInicial = double.Parse(Console.ReadLine());
+
+            Console.Write("Insira a quilometragem final: ");
+            double quilometragemFinal = double.Parse(Console.ReadLine());
+
+            Console.Write("Insira a quantidade de combustível gasta em litros: ");
+            double combustivelGasto = double.Parse(Console.ReadLine());
+
+            double KmPorLitro = (quilometragemFinal - quilometragemInicial) / combustivelGasto;
+
+            Console.WriteLine($"\nO carro fez {KmPorLitro.ToString("F2")}km/l");
+        }
+    }
+}
