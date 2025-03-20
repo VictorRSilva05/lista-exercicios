8f1f7f96fe12d66ab34d9e0296519fb7a6e17967
Victor R. Silva <victorr.silva005@gmail.com>
2025-03-20 15:02:52 -0300

    Exercício 5 concluido

---
+++ Exercicio5/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercicio5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira o salário do vendedor: ");
+            decimal salario = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Insira o valor total das vendas realizadas pelo vendedor: ");
+            decimal valorTotalVendas = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Insira a porcentagem de comissão do vendedor: ");
+            decimal porcentagemComissao = decimal.Parse(Console.ReadLine());
+
+            decimal comissao = valorTotalVendas * (porcentagemComissao / 100);
+            decimal salarioFinal = salario + comissao;
+
+            Console.WriteLine($"\nO salário final do vendedor é R${salarioFinal.ToString("F2"}");
+
+        }
+    }
+}
